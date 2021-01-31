0126b5e62c5e5dee62e5d9a98757cee5f8137221
Artem Sivyankov <morfiy0393@gmail.com>
2021-02-01 00:19:16 +0300

    init: statrt task reverse int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let nbr = n.toString().split("");
+    if (nbr[0] == `-`) {
+        nbr.shift();
+        return Number(nbr.reverse().join(""));
+    } else {
+        return Number(n.toString().split("").reverse().join(""));
+    }
+};
+
+// function reverse(n) {
+//     let nbr = n.toString().split("");
+//     if (nbr[0] == `-`) {
+//         nbr.shift();
+//         return Number(nbr.reverse().join(""));
+//     } else {
+//         return Number(n.toString().split("").reverse().join(""));
+//     }
+// }
+
+// reverse(-12345);
+// reverse(5678);
